0cbe0974603fb5c18162fad4656e33552317fab3
xProsek720 <83226439+xProsek720@users.noreply.github.com>
2023-03-29 20:04:19 +0200

    template

---
+++ includes/imports.h
@@ -0,0 +1,19 @@
+//
+// Created by xProsek on 29.03.2023.
+//
+
+#pragma once
+
+#ifndef GRA_IMPORTS_H
+#define GRA_IMPORTS_H
+
+// Tutaj dajemy importy, żeby zachować jakiś porządek
+
+//Z C++
+#include <iostream>
+
+//Z SFML
+
+//inne-dodane ( proszę skonsultowac to na d.gg! :D )
+
+#endif //GRA_IMPORTS_H
